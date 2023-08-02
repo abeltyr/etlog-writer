debd9f3696da7d202c98cf4eaeffb85ead360c60
abeltyr <abeltyr@gmail.com>
2023-08-02 19:49:04 +0300

    (feat) on paste update

--- src/utils/actions/onpaste.tsx
+++ src/utils/actions/onpaste.tsx
@@ -8,28 +8,28 @@ export const onPaste = async ({
     onUpdate: Function
 }) => {
 
-    event.preventDefault();
+    // event.preventDefault();
 
-    const text = event.clipboardData.getData('text/plain');
-    const selection = window.getSelection();
-    if (selection) {
-        if (!selection.rangeCount) return false;
-        selection.deleteFromDocument();
-        const range = selection.getRangeAt(0);
-        const textNode = document.createTextNode(text);
-        range.insertNode(textNode);
+    // const text = event.clipboardData.getData('text/plain');
+    // const selection = window.getSelection();
+    // if (selection) {
+    //     if (!selection.rangeCount) return false;
+    //     selection.deleteFromDocument();
+    //     const range = selection.getRangeAt(0);
+    //     const textNode = document.createTextNode(text);
+    //     range.insertNode(textNode);
 
-        // Move the caret to the end of the inserted text
-        range.setStartAfter(textNode);
-        range.setEndAfter(textNode);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
+    //     // Move the caret to the end of the inserted text
+    //     range.setStartAfter(textNode);
+    //     range.setEndAfter(textNode);
+    //     selection.removeAllRanges();
+    //     selection.addRange(range);
+    // }
 
 
-    const el: any = document.getElementById(id);
+    // const el: any = document.getElementById(id);
 
-    if (el) {
-        onUpdate(el.textContent)
-    }
+    // if (el) {
+    //     onUpdate(el.textContent)
+    // }
 };
